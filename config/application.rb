07837b1d76fcb222c73c078f4bf02aca8afe4058
ayslanmarcelino <ayslan_marcelino@outlook.com>
2020-12-26 13:47:29 -0300

    feature/add-enterprise-and-users-model Add devise and login page view
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- config/application.rb
+++ config/application.rb
@@ -17,12 +17,12 @@ Bundler.require(*Rails.groups)
 
 module MySales
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators.system_tests = nil
+    config.time_zone = 'America/Maceio'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ['pt-BR']
+    config.i18n.default_locale = :'pt-BR'
   end
 end
