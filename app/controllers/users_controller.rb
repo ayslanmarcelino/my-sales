17a3933b36e8cc4fd4931ed69f212880f980ab4b
ayslanmarcelino <ayslan_marcelino@outlook.com>
2020-12-27 21:19:43 -0300

    feature/add-enterprise-and-users-model Add is active user conditional
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_active_or_employee?
+
+  private
+
+  def is_active_or_employee?
+    redirect_to root_path, notice: 'Você não tem permissão para acessar esta página' if current_user.is_employee == false || current_user.is_active == false
+  end
 end
