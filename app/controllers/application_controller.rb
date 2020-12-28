a6b0ea943ddaae847db94651a3244a7a0e1f1a30
ayslanmarcelino <ayslan_marcelino@outlook.com>
2020-12-28 13:13:28 -0300

    feature/add-users-crud Add edit my profile
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :is_active?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def is_active?
     if current_user.present? && !current_user.is_active?
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
       flash[:danger] = 'Sua conta está inativa. Para mais informações, entre em contato com o administrador da empresa.'
       root_path
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name nickname document_number])
   end
 end
